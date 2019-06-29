97063263005b0f9809dad95d6aa62e4bf68a18d8
Lachlan Young <lachlan_young@hotmail.com>
2019-06-29 15:30:07 +1000

    hello and bye for Brian

--- server/handler.js
+++ server/handler.js
@@ -1,12 +1,26 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+const hello = async (event, context) => {
   const { name } = event.pathParameters
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Go Serverless v1.0! Your function executed successfully! Hello ${name}`,
-      input: event,
+      message: `Hello ${name}`,
     }),
   };
 };
+
+const bye = async (event, context) => {
+  const { name } = event.pathParameters
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello ${name}`,
+    }),
+  };
+};
+
+module.exports = {
+  hello,
+  bye
+}
