3ece02507a521855b6f42f9ebe20abb9bade7c53
Lachlan Young <lachlan_young@hotmail.com>
2019-06-29 14:31:05 +1000

    probabilities view

---
+++ client/src/views/probabilities/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Probabilities extends Component {
+  render() {
+      return (
+          <div>
+            Hi
+          </div>
+      );
+  }
+}
