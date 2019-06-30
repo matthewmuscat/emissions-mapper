ec998b9465909318eefcd1cd770ae86820c9c026
Matthew Muscat <matthewmuscat@Matthews-MacBook-Pro.local>
2019-06-30 16:56:54 +1000

    wont re-render

--- client/src/views/worldmap/WorldMap.jsx
+++ client/src/views/worldmap/WorldMap.jsx
@@ -34,11 +34,12 @@ class WorldMap extends Component {
   async componentWillMount() {
     const SERVER_URL='https://9fwwprofzl.execute-api.ap-southeast-2.amazonaws.com/dev'
     const { year } = this.state;
-    const emissionData = await Api(`${SERVER_URL}/getEmissions/${year}`, 'GET');
+    // const emissionData = await Api(`${SERVER_URL}/getEmissions/${year}`, 'GET');
     const rangeData = await Api(`${SERVER_URL}/getRange/${year}`, 'GET');
 
+    await this.getEmissions();
     this.setState({
-      data: emissionData.body,
+      // data: emissionData.body,
       range: rangeData.body[0]
     })
   }
@@ -50,15 +51,20 @@ class WorldMap extends Component {
     return emissionCount;
   }
 
+  getEmissions = async () => {
+    const SERVER_URL='https://9fwwprofzl.execute-api.ap-southeast-2.amazonaws.com/dev'
+    const { year, data } = this.state;
+    const emissionData = await Api(`${SERVER_URL}/getEmissions/${year}`, 'GET');
+
+    console.log('updating data: ', emissionData.body[0])
+    this.setState({ data: emissionData.body })
+  }
+
   renderHeatmap = (country) => {
     const { year, range, data} = this.state; 
+    const { max, min } = range;
 
     const emissionCount = data.length && this.calculateEmissions(country);
-    const max = range.max;
-    const min = range.min;
-
-    if(emissionCount === max) console.log('found country: ', country);
-    
     const maxShorten = emissionCount/max * 100;
 
     const red = 'rgb(255, 0, 0)';
@@ -66,86 +72,77 @@ class WorldMap extends Component {
     const green = 'rgb(0, 255, 0)';
     const grey = 'rgb(66, 66, 66)';
 
-    console.log('maxShorten is: ', maxShorten);
-
     if(maxShorten < 15) {
-      return green
+        return green
     } else if (maxShorten < 30) {
-      return yellow;
+        return yellow;
     } else if( maxShorten > 30) {
-      return red;
+        return red;
     } else {
-      return grey;
+        return grey;
     }
-    
-    // switch (maxShorten) {
-    //   case (maxShorten < 15) : {
-        
-    //   }
-    //   case (maxShorten < 15 && maxShorten > 1) : 
-    //     return yellow;
-    //   case (maxShorten < 50 && maxShorten > 15) :
-    //     return red;
-    //   default:
-    //     return grey;
-    // }
   }
   
-  updateYear = (event, value) => {
+  updateYear = async (event, value) => {
+    await this.getEmissions();
     this.setState({ year: value })
   }
 
   render() {
       const { classes } = this.props;
-      const { data } = this.state;
-        console.log('data.length: ', data.length);
-        // console.log(this.renderHeatmap("Australia"));
-        
+      const { data, year } = this.state;
+      console.log('year: ', year);
+
+      console.log('im gonna re-render');
       return (
         <div className={classes.root}>
           {
             data.length && (
               <ComposableMap
-              projectionConfig={{
-                scale: 205,
-                rotation: [-11,0,0],
-              }}
-              width={980}
-              height={551}
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
+                projectionConfig={{
+                  scale: 205,
+                  rotation: [-11,0,0],
+                }}
+                width={980}
+                height={551}
+                style={{
+                  width: "100%",
+                  height: "auto",
+                }}
               >
               <ZoomableGroup center={[0,20]}>
                 <Geographies geography={ "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/examples/choropleth-map/static/world-50m-with-population.json" }>
-                  {(geographies, projection) => geographies.map((geography, i) => (
-                    <Geography
-                      key={ i }
-                      geography={ geography }
-                      projection={ projection }
-                      style={{
-                        default: {
-                          stroke: '#000',
-                          strokeWidth: 0.5,
-                          outline: "none",
-                          fill: this.renderHeatmap(geography.properties.name)
-                        },
-                        hover: {
-                          fill: "#263238",
-                          stroke: "#fff",
-                          strokeWidth: 1.5,
-                          outline: "none",
-                        },
-                        pressed: {
-                          fill: "#263238",
-                          stroke: "#607D8B",
-                          strokeWidth: 0.75,
-                          outline: "none",
-                        }
-                      }}
-                    />
-                  ))}
+                  {(geographies, projection) => geographies.map((geography, i) => {
+                      console.log('found data');
+                      return (
+                        <Geography
+                            key={ i }
+                            geography={ geography }
+                            projection={ projection }
+                            style={{
+                            default: {
+                                stroke: '#000',
+                                strokeWidth: 0.5,
+                                outline: "none",
+                                fill: this.renderHeatmap(geography.properties.name)
+                            },
+                            hover: {
+                                fill: "#263238",
+                                stroke: "#fff",
+                                strokeWidth: 1.5,
+                                outline: "none",
+                            },
+                            pressed: {
+                                fill: "#263238",
+                                stroke: "#607D8B",
+                                strokeWidth: 0.75,
+                                outline: "none",
+                            }
+                            }}
+                        />
+                      )
+                    }
+                  )}
                 </Geographies>
               </ZoomableGroup>
             </ComposableMap>
