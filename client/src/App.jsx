a73830be1b30c9e6e8e9c02412e65c15a6f948ca
Matthew Muscat <matthewmuscat@Matthews-MacBook-Pro.local>
2019-06-30 12:07:03 +1000

    Add slider to map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 // Import Views:
-import WorldMap from './views/worldmap/index';
+import WorldMap from './views/worldmap/WorldMap';
+import SliderComponent from './views/components/SliderComponent'
 
 class App extends Component {
   render() {
       return (
           <div>
             <WorldMap/>
+            <SliderComponent/>
           </div>
       );
   }
